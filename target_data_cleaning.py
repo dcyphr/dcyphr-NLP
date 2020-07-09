0c8e1257d397fd01e35ca8b271d97950ffa5942a
Jeff Ma <jeffma@Jeffs-MacBook-Pro.local>
2020-07-08 20:27:50 -0400

    basic cleaning installed; need to fix inline style split and weird spacing

--- target_data_cleaning.py
+++ target_data_cleaning.py
@@ -1,11 +1,55 @@
 import re
+import os
+import sys
+import argparse
+import csv
 
-filepath = "summary.txt"
-with open(filepath) as fp:
-    lines = fp.readlines()
-    for line in lines:
-        # TODO 
-        # separate each new section (delimited by <h2>) into new line
-        # add delimiter " | " after </h2>
-        # remove html tags
-        return
+def make_file(filepath, outpath):
+    clean = re.compile('<.*?>|&nbsp;')
+    maxInt = sys.maxsize
+
+    while True:
+        # decrease the maxInt value by factor 10
+        # as long as the OverflowError occurs.
+
+        try:
+            csv.field_size_limit(maxInt)
+            break
+        except OverflowError:
+            maxInt = int(maxInt/10)
+
+    with open(filepath, newline='') as csvfile:
+        file_reader = csv.reader(csvfile, delimiter=' ')
+        for row in file_reader:
+            data = row[0]
+            sections = re.split('<h2(.*?)>', data)
+            for section in sections:
+                section = section.replace('</h2>', ' |')
+                section = re.sub(clean, '', section)
+                target = open(outpath, 'a')
+                target.write(section + ' \n')
+                target.close()
+
+def main():
+    parser = argparse.ArgumentParser(
+        description = 'Transforms dcyphr csv to target text file',
+        formatter_class = argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('src', type = str,
+                        help = 'Path to input csv')
+    parser.add_argument('out', type = str,
+                        help = 'Path to output csv')
+    
+    args = parser.parse_args()
+
+    filepath = args.src
+    outpath = args.out
+
+    if os.path.isfile(outpath):
+        os.remove(outpath)
+    open(outpath, 'a').close()
+
+    make_file(filepath, outpath)
+
+if __name__ == '__main__':
+    main()
