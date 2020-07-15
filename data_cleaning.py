de8a5cddc8eefba18281ca7a872b4a062e05f10f
Jeff Ma <jeffma@Jeffs-MacBook-Pro.local>
2020-07-15 18:48:45 -0400

    completed starter script for merge

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,12 +2,33 @@ import os
 import sys
 import re
 
-def make_file(filepath, source_out, target_out):
-    # compiled regex to remove HTML tags, nbsp, style attributes, percent signs, excessive spacing, and newlines
-    clean = re.compile("""<.*?>|&nbsp;|style=("|').*?("|')|%|([ ]|[\t]){2,}|\r?\n|\r""")
-    maxInt = sys.maxsize
+# compiled regex to remove HTML tags, nbsp, style attributes, percent signs, excessive spacing, and newlines
+clean = re.compile("""<.*?>|&nbsp;|style=("|').*?("|')|%|([ ]|[\t]){2,}|\r?\n|\r""")
+ 
+def scrape_function(link):
+    # TODO
+    """
+    Write function to scrape links and return dictionary of {section_header:section_text}
+    """
+    return
 
-    while True:
+def clean_target(summary):
+    sections = re.split('<h2.*?>', summary)
+    output = {}
+    for section in sections:
+        print(section)
+        if section!='':
+            temp = re.split('</h2>', section)
+            section_heading = re.sub(clean, '', temp[0]) 
+            section_text = re.sub(clean, '', temp[1])
+            output[section_heading] = section_text
+    return output
+
+def make_file(filepath, source_out, target_out):
+
+   maxInt = sys.maxsize
+
+   while True:
         # decrease the maxInt value by factor 10
         # as long as the OverflowError occurs.
 
@@ -18,7 +39,7 @@ def make_file(filepath, source_out, target_out):
             maxInt = int(maxInt/10)
 
     # opens csv file
-    with open(filepath, newline='') as csvfile:
+   with open(filepath, newline='') as csvfile:
         file_reader = csv.reader(csvfile, delimiter=' ')
 
         # for each row in csv, split up by section
@@ -26,14 +47,20 @@ def make_file(filepath, source_out, target_out):
             souce_article_link = row[0]
             target_summary = row[1]
 
-            sections = re.split('(<h2.*?>)', target_summary) # regex to split by section
-            for section in sections:
-                # TODO
-                """Check if section scraped from links matches with sections in summary"""
-                section = section.replace('</h2>', ' | ') # replaces closing tag with delimiter 
-                section = re.sub(clean, '', section) # cleans section using compiled regex from above
-                target = open(target_out, 'a') # opens and writes into new file
-                target.write(section + ' \n')
-                target.close()
+            # TODO
+            """
+            Scrape from source_article_link
+            Check if section scraped from links matches with sections in summary
+            """
 
+            sections_original = scrape_function(source_article_link) # outputs a dict
+            sections_target = clean_target(target_summary) # outputs a dict
+            
+
+
+def main():
+    print(clean_target("<h2 style=blah>Abstract</h2><p>This is some text</p><h2>Introduction</h2><span>blah blah</span>"))
+
+if __name__ == '__main__':
+    main()
 
