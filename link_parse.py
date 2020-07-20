6d330c201f1f92e7564a9fa147567c72c574abaa
Maitreyi Chatterjee <mc2259@cornell.edu>
2020-07-20 17:06:09 +0530

    Changed link_parse.py

--- link_parse.py
+++ link_parse.py
@@ -12,6 +12,117 @@ with open('links.csv','r') as file:
   for row in reader:
     lst.append(row[0])
 
+def cell_extract():
+  """Returns a list of extract from cell articles """
+  dicti={}
+  listi=[]
+  for i in cell:
+    result=requests.get(i)
+    src=result.content
+    soup= BeautifulSoup(src,'lxml')
+    match1=soup.find_all(class_='sectionTitle')
+    match=soup.find_all('div',class_='section-paragraph')
+    # match=soup.find_all('section')
+    if(match1==[]):
+
+      dicti['Abstract:']=(match[0].text)
+      listi.append(dicti)
+
+    else:
+      dicti={}
+      for j in range(len(match1)):
+        
+        dicti[(match1[j].text)]=(match[j].text)
+      listi.append(dicti)
+
+  
+  return listi
+
+def ncbi_pubmed_extract():
+  """ Gets the abstracts """
+  listi=[]
+  for i in ncbi:
+    dicti={}
+    result=requests.get(i)
+    src=result.content
+    soup= BeautifulSoup(src,'lxml')
+    match=soup.find_all('div',class_='abstract-content selected')
+    for j in match:
+      dicti['Abstract']=(j.text.strip())
+    listi.append(dicti)
+  return listi
+
+
+def njem_extract():
+  """Extracts njem articles """
+  listi=[]
+  for i in nejm:
+    dicti={}
+    result = requests.get(i)
+    src = result.text
+    soup = BeautifulSoup(src, 'lxml')
+    match=soup.find_all('section',class_='o-article-body__section')
+    for i in match[:5]:
+      k=len((i.text.split()[0]))
+      dicti[(i.text.split()[0])]=i.text[k+1:].strip()
+    
+      if 'Abstract' not in dicti.keys():
+        match2=soup.find_all('p',class_='f-body')
+        dicti={}
+        s=""
+        for i in match2:
+          s+=i.text
+        dicti['Abstract']=s
+    listi.append(dicti)
+  return listi
+
+
+def pmc_extract():
+  dicti=[]
+  m=['Abstract','Introduction']
+  for i in pmc:
+    result = requests.get(i)
+    src = result.text
+    soup = BeautifulSoup(src, 'lxml')
+
+    match = soup.find_all('div',class_="tsec sec")
+    for i in range(len(match[:5])):
+      
+      dicti.append(match[i].text)
+      
+      
+  for i in dicti:
+
+    print(i)
+    print('\n')
+    print('\n')
+  # print(match2)
+  # for k in range(len(match)):
+  #   print(match2[k].text)
+  #   print(match[k].text)
+
+def scimag():
+  for i in sci_direct:
+    result=requests.get(i)
+    src = result.text
+    soup=BeautifulSoup(src, 'lxml')
+
+
+    match = soup.find_all('div',class_='section abstract')
+    match2 = soup.find_all('div',class_='section discussion')
+    match3 = soup.find_all('div',class_='section conclusions')
+   
+    for j in match:
+      print(j.text)
+    for k in match2:
+      print(k.text)
+
+    for f in match3:
+      print(f.text)
+
+    
+
+
 def get_sim():
   """Gets similar articles """
   sim=soup.find('div',class_='similar-articles') 
