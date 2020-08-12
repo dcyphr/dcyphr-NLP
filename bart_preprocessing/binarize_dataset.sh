b488c648cbb6ec83498096cee23f65d54590ec4c
Shardul Bansal <bansa11750@gmail.com>
2020-08-12 13:18:41 +0200

    cleaning repo

--- bart_preprocessing/binarize_dataset.sh
+++
@@ -1,9 +0,0 @@
-fairseq-preprocess \
-  --source-lang "source" \
-  --target-lang "target" \
-  --trainpref "${TASK}/train.bpe" \
-  --validpref "${TASK}/val.bpe" \
-  --destdir "${TASK}-bin/" \
-  --workers 60 \
-  --srcdict dict.txt \
-  --tgtdict dict.txt;
